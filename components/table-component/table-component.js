5fb9a08c7256bb8e4d8038683cb6cd50185e6312
jinuk05006 <jinuk05006@gmail.com>
2024-11-14 15:31:09 +0800

    added intro until value prop

--- components/table-component/table-component.js
+++ components/table-component/table-component.js
@@ -9,6 +9,19 @@ new gridjs.Grid({
   ],
 }).render(document.getElementById("table-1"));
 
+new gridjs.Grid({
+  columns: ["Section", "Description", "Marks allocation"],
+  data: [
+    ["Science Mission", "Assesses the rover's capability to analyze Martian-like environments for signs of life. Key components include site investigation, sample collection, onboard analysis and data documentation.", "100 points"],
+    ["Delivery Mission", "Evaluates the rover’s ability to locate, pick up and deliver objects across rugged terrain, which simulates assistance to astronauts. A drone may assist in this mission.", "100 points"],
+    ["Equipment Servicing", "Tests the rover’s dexterity in operating equipment on a mock lander. Tasks include manipulating small objects, operating controls and completing maintenance operations.", "100 points"],
+    ["Autonomous Navigation", "Focuses on the rover’s autonomous capabilities to navigate between GPS and visual markers without human intervention, including object recognition and obstacle avoidance.", "100 points"],
+    ["System Acceptance Review", "Prior to competition, teams submit a technical review covering rover design, functionality and mission-specific capabilities. Scores affect eligibility to compete in the field challenge.", "100 points"],
+  ],
+  
+}).render(document.getElementById("table-2"));
+
+
 class TableComponent extends HTMLElement {
   static get observedAttributes() {
     return ["subtitle"];
